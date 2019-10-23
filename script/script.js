ee21981201b9013f0de1dd58889c9324d544a480
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-23 21:33:00 +0530

    passing the user input

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
+let originalWeight
+
+
+
 init();
 
 
 function init(){
     document.getElementsByClassName("save-button")[0].addEventListener("click", function(){
+        originalWeight = document.getElementsByClassName("weight")[0].value;
+        document.getElementsByClassName("original-weight")[0].innerHTML = originalWeight;
         $(".program-selection").show();
         $(".main").hide();
     })
