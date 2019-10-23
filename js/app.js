6d8166578879eee427836d9516dc3f3ddaf689b9
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-10-23 10:45:09 +0530

    updates

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function process(){
+    let name=document.getElementsByClassName[0]("name");
+    let age=document.getElementsByClassName[0]("age");
+    let ReEnteredPass=document.getElementsByClassName[0]("height");
+}
