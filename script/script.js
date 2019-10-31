6dc17a7e07fb99041a103cf50732417935758494
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-31 18:07:31 +0530

    setting the condition for the desired weight

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+let name 
+let age
+let height
 let originalWeight
 let desiredWeight
 
@@ -12,28 +15,30 @@ function init(){
         $(".program-selection").hide();
     })
 
-
     document.getElementsByClassName("save-button")[0].addEventListener("click", function(){
-        originalWeight = document.getElementsByClassName("weight")[0].value;
+        name = $(".name").val();
+        age = $(".age").val();
+        height = $(".height").val();
+        originalWeight = $(".weight").val();
         document.getElementsByClassName("original-weight")[0].innerHTML = originalWeight;
-        console.log("hey");
-        
         $(".program-selection").show();
         $(".main").hide();
 
          console.log(originalWeight);
          
-        let g = {}
-        g.originalWeight = originalWeight;
-        
+        let userDetails = {}
+        userDetails.name = name;
+        userDetails.age = age;
+        userDetails.height = height;
+        userDetails.originalWeight = originalWeight;
         $.ajax({
             type: "POST",
-            data: JSON.stringify(g),
+            data: JSON.stringify(userDetails),
             contentType:"application/json",
             url: "http://localhost:3000/api",
             success: (data) => {
-                console.log("Heyyy");
-                window.location.href='/src/html/dashboard.html'
+                console.log(userDetails);
+                // window.location.href='/src/html/dashboard.html'
             },
             error: () => {
                 console.log("Error");
@@ -45,13 +50,50 @@ function init(){
         });
     })
 
+     desiredWeightGoal();
+}
+
+function desiredWeightGoal(){    
+    let a,b;
     document.getElementsByClassName("basic")[0].addEventListener("click", function () {
         desiredWeight = document.getElementsByClassName("desired-weight")[0].value;
         console.log(desiredWeight);
+        let userDetails = {}
+        userDetails.desiredWeight = desiredWeight;
+        if(originalWeight > desiredWeight){
+            a = originalWeight
+            b = desiredWeight;
+        }
+        else{
+            b = originalWeight
+            a = desiredWeight;   
+        }
+
+        if( ((a - b) > 0 ) && ((a -b) <=20 ) ){
+
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(userDetails),
+                contentType:"application/json",
+                url: "http://localhost:3000/api/desiredWeight",
+                success: (data) => {
+                    console.log(userDetails);
+                    // window.location.href='/src/html/dashboard.html'
+                },
+                error: () => {
+                    console.log("Error");
+                    
+                },
+                complete: () => {
+                console.log("complete");
+                }
+            });
+        }
+    else{
+        alert("Enter the value correctly")
+    }
     })
 }
-
-
 
 
 //for displaying the time
