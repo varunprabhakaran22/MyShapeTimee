9da538453eae452ab070c1534877cfbde5f7d0e7
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-31 14:47:57 +0530

    sending the data from client to server

--- script/script.js
+++ script/script.js
@@ -2,9 +2,9 @@ let originalWeight
 let desiredWeight
 
 
-getDay();
-init();
 
+init();
+getDay();
 
 function init(){
     
@@ -16,18 +16,42 @@ function init(){
     document.getElementsByClassName("save-button")[0].addEventListener("click", function(){
         originalWeight = document.getElementsByClassName("weight")[0].value;
         document.getElementsByClassName("original-weight")[0].innerHTML = originalWeight;
+        console.log("hey");
+        
         $(".program-selection").show();
         $(".main").hide();
+
+         console.log(originalWeight);
+         
+        let g = {}
+        g.originalWeight = originalWeight;
+        
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify(g),
+            contentType:"application/json",
+            url: "http://localhost:3000/api",
+            success: (data) => {
+                console.log("Heyyy");
+                window.location.href='/src/html/dashboard.html'
+            },
+            error: () => {
+                console.log("Error");
+                
+            },
+            complete: () => {
+               console.log("complete");
+            }
+        });
     })
 
     document.getElementsByClassName("basic")[0].addEventListener("click", function () {
         desiredWeight = document.getElementsByClassName("desired-weight")[0].value;
         console.log(desiredWeight);
     })
-
-    //calling the day function
-  
 }
+
+
 
 
 //for displaying the time
@@ -39,7 +63,6 @@ function getDay(){
     month = months[month]
     let dayValue=date.getDate(); 
     dayValue = dayValue + " " + month;
-    console.log(dayValue);
-     
+    console.log(dayValue);     
     document.getElementsByClassName("calender")[0].innerHTML=dayValue;
 }
