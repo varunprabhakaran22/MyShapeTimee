378cb876ad59fbe656957e4333e3a54811142deb
AadeshNichite <54974312+AadeshNichite@users.noreply.github.com>
2019-11-05 13:19:38 +0530

    updates

--- js/app.js
+++ js/app.js
@@ -4,48 +4,41 @@ let Weight;
 let height;
 let password;
 let data;
+let Repassword;
 
 function uploadData(){
 
     name=document.getElementsByClassName("name");
+    email=document.getElementsByClassName("email");
     age=document.getElementsByClassName("age");
     height=document.getElementsByClassName("height");
     Weight=document.getElementsByClassName("weight");
+    password=document.getElementsByClassName("password");
+    Repassword=document.getElementsByClassName("Re-password");
 
+    var atposition=email[0].value.indexOf("@");  
+    var dotposition=email[0].value.lastIndexOf(".");  
+
+    if(password==Repassword && atposition<1 || dotposition<atposition+2 || dotposition+2>=name[0].value.length)
+    {
        $.ajax({
         url: 'http://localhost:8000/add',
         type: 'POST',
         dataType: 'json',
         data: { 
          'name': name[0].value, 
+         'email':email[0].value,
          'age' : age[0].value,
          'height' : height[0].value,
-         'Weight' : Weight[0].value
+         'Weight' : Weight[0].value,
+         'password':password[0].value
         } ,
         success: function (response) {
             alert(response.status);
         }
     }); 
+    }
 
-}
-
-function uploadSignUpData()
-{
-    name=document.getElementsByClassName("name");
-    password=document.getElementsByClassName("password");
-
-    $.ajax({
-        url: 'http://localhost:8000/signup',
-        type: 'POST',
-        dataType: 'json',
-        data: { 
-         'name': name[0].value, 
-         'password' : password[0].value,
-        } ,
-        success: function (response) {
-            alert(response.status);
-        }
-    }); 
 }
 
 function checkLogin()
@@ -81,7 +74,6 @@ function checkLogin()
    
     
 }
-
 
 
 $(document).ready(function(){
