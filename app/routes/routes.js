c1cfcbe7dc22dfc5adaa8e988f0e2e331c360345
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-06 17:22:51 +0530

    using the object calling the function

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -141,7 +141,8 @@ module.exports = (app, db) => {
                         res.status(200).json({msg:"User Exist"});
                         console.log("am coming");
                         
-                        let userMenu = new Menu(userData, menu , menuPerDay)
+                        let userMenu = new Menu(userData, menu , menuPerDay);
+                        userMenu.calculatingBmi();
                         userMenu.calculateMenuPerDay();
                     }
                 });
