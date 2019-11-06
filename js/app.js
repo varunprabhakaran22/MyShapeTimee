5aa5e33d1d7b3b14ca5ff3e4e1f982bb98d4c845
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-06 15:46:50 +0530

    formating the code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { inherits } from "util";
+
 let name;
 let age;
 let Weight;
@@ -7,8 +9,10 @@ let data;
 let Repassword;
 let DesiredWeight;
 
+
+
+// getting the user profile data from client using ajax call 
 function uploadData(){
-    
     name=document.getElementsByClassName("name");
     email=document.getElementsByClassName("email");
     age=document.getElementsByClassName("age");
@@ -17,9 +21,8 @@ function uploadData(){
     DesiredWeight=document.getElementsByClassName("DesiredWeight");
     password=document.getElementsByClassName("password");
     Repassword=document.getElementsByClassName("Re-password");
-    
-    var atposition=email[0].value.indexOf("@");  
-    var dotposition=email[0].value.lastIndexOf(".");  
+    let atposition=email[0].value.indexOf("@");  
+    let dotposition=email[0].value.lastIndexOf(".");  
 
     if(Weight > DesiredWeight){
         a = Weight
@@ -29,85 +32,76 @@ function uploadData(){
         b = Weight
         a = DesiredWeight;
     }      
-   
 
     if((name[0].value=="" && email[0].value=="" && age[0].value=="" && height[0].value=="" && Weight[0].value==""
     && password[0].value==""))
     {
-        alert("Fill the data..");
-       
+        alert("Fill the data..");    
     }
+
     else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email[0].value.length){  
-
         alert("Please enter a valid Email"); 
-    
     }
-    else if(!(password[0].value === Repassword[0].value)){
 
+    else if(!(password[0].value === Repassword[0].value)){
         alert("Password does not matchs"); 
     }
+
     else if ( ((a - b) < 0 ) && ((a -b) >= 20 ) ){
-
         alert("Enter valid Desired Weight.."); 
-
     }
-    else{
-       
-       $.ajax({
-        url: 'http://localhost:8000/add',
-        type: 'POST',
-        dataType: 'json',
-        data: { 
-         'name': name[0].value, 
-         'email':email[0].value,
-         'age' : age[0].value,
-         'height' : height[0].value,
-         'Weight' : Weight[0].value,
-         'DesiredWeight' : DesiredWeight[0].value,
-         'password':password[0].value
-        }
-         }).done(function(data){
-                if(data.msg=="success")
-                {
-                    location.replace("login.html");
-                }
-                else if(data.msg=="Email Id already present")
-                {
-                    alert("Email ID Already Present")
-                }
+
+    else{   
+        $.ajax({
+            url: 'http://localhost:8000/add',
+            type: 'POST',
+            dataType: 'json',
+            data: { 
+                'name': name[0].value, 
+                'email':email[0].value,
+                'age' : age[0].value,
+                'height' : height[0].value,
+                'Weight' : Weight[0].value,
+                'DesiredWeight' : DesiredWeight[0].value,
+                'password':password[0].value
+            }
+        })
+        .done(function(data){
+            if(data.msg=="success")
+            {
+                location.replace("login.html");
+            }
+            else if(data.msg=="Email Id already present")
+            {
+                alert("Email ID Already Present")
+            }
         }); 
     }
-
 }
 
-function checkLogin()
-{
+function checkLogin(){
     email=document.getElementsByClassName("email");
     password=document.getElementsByClassName("password");
- 
-    var atposition=email[0].value.indexOf("@");  
-    var dotposition=email[0].value.lastIndexOf(".");  
-
+    let atposition=email[0].value.indexOf("@");  
+    let dotposition=email[0].value.lastIndexOf(".");  
     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email[0].value.length){  
-
         alert("Please enter a valid e-mail address");  
-      
     }
     else
     {
-          $.ajax({
+        $.ajax({
             url: 'http://localhost:8000/',
             type: 'POST',
             dataType: 'json',
             data: { 
-            'email': email[0].value, 
-            'password' : password[0].value,
+                'email': email[0].value, 
+                'password' : password[0].value,
             }
-        }).done(function(data){
+        })
+        .done(function(data){
             if(data.msg=="User Exist")
             {
                 location.replace("Dashboard.html");
-                //console.log(data);
             }
             else if(data.msg=="User Does Not Exist")
             {
@@ -118,7 +112,6 @@ function checkLogin()
             {
                 alert("User Does Not Exist");
             }
-         }); 
+        }); 
     }  
 }
-
