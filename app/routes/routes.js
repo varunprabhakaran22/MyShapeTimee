e9ed1041ee2a415ded16c13618b1c405afde00b2
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-14 22:06:17 +0530

    remove

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const {Menu} = require("../../controller/controller");
-
+const menu = require('./menudata')
 
 // declaring the global variables
 let userData;
