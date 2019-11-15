28382039a3be0b19ba0f15101a377b423adc6602
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-15 10:21:55 +0530

    exporting the menu file

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const {Menu} = require("../../controller/controller");
+const menu = require('../../menudata')
 // const menu = require('...me')
-
+console.log(menu);
 // declaring the global variables
 let userData;
 let eggCount = 0;
@@ -17,118 +18,6 @@ let menuPerDay={
 	snacks: []   
 }
 let UserWeight = 0
-let menu = {
-	"breakfast": [{
-			"Name": "Egg",
-			"Protein": 5,
-			"Calorie": 78
-		},
-		{
-			"Name": "Milk",
-			"Protein": 8,
-			"Calorie": 42
-		},
-		{
-			"Name": "Banana",
-			"Protein": 1.3,
-			"Calorie": 100
-		},
-		{
-			"Name": "OatMmeals",
-			"Protein": 17,
-			"Calorie": 75
-		},
-		{
-			"Name": "Wheat Bread",
-			"Protein": 3.6,
-			"Calorie": 75
-		}
-	],
-	
-	"snacks" :[{
-			"Name": "Egg",
-			"Protein": 5,
-			"Calorie": 78
-		},
-		{
-			"Name": "Apple",
-			"Protein": 5,
-			"Calorie": 150
-		},
-		{
-			"Name": "Turkey with cheese ",
-			"Protein": 5,
-			"Calorie": 140
-		},
-		{
-			"Name": "Cereal",
-			"Protein": 5,
-			"Calorie": 180
-		},
-		{
-			"Name": "Nuts",
-			"Protein": 7,
-			"Calorie": 160
-		}
-	],
-
-	"lunch" :[{
-			"Name": "Chicken Breast",
-			"Protein": 20,
-			"Calorie": 165
-		},
-		{
-			"Name": "Brown Rice",
-			"Protein": 3,
-			"Calorie": 111
-		},
-		{
-			"Name": "Spinach",
-			"Protein": 2.9,
-			"Calorie": 100
-		},
-		{
-			"Name": "Salmon",
-			"Protein": 20,
-			"Calorie": 185
-		},
-		{
-			"Name": "Tuna",
-			"Protein": 20,
-			"Calorie": 180
-		}
-	],
-	
-	"dinner" :[{
-			"Name": "Nuts",
-			"Protein": 7,
-			"Calorie": 160
-		},
-		{
-			"Name": "Beans",
-			"Protein": 8,
-			"Calorie": 347
-		},
-		
-		
-		{
-			"Name": "Avacados",
-			"Protein": 2.5,
-			"Calorie": 160
-		},
-		{
-			"Name": "Potato",
-			"Protein": 2.5,
-			"Calorie": 77
-		},
-		
-		{
-			"Name": "Chapathi",
-			"Protein": 3.5,
-			"Calorie": 200
-		}
-	]
-}
 
 
 // getting user details and storing the details to mongodb
