e3ac2fc5c91e83209d75546f0adec0413a5ff21b
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-11-19 01:02:12 +0530

    Adding the comments and removing the dead codes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,4 @@
-//importing all the documents
+//importing all the required middlewares, packages and class
 const express = require('express');
 const router = express.Router();
 const {Menu} = require("../../controller/controller");
@@ -12,16 +12,16 @@ let eggCount = 0;
 let updatedWeight = 0;
 let numberOfDayMenuTook = 0;
 let dayCount=0;
+let UserWeight = 0
 let menuPerDay={
 	breakfast: [],
 	lunch: [],
 	dinner:[],
 	snacks: []   
 }
-let UserWeight = 0
 
 
-// getting user details and storing the details to mongodb
+//Getting user profile details and storing the details to mongodb
 module.exports = (app, db) => {   
     app.post("/add", (req, res) => {
 		console.log(req.body);
@@ -29,8 +29,9 @@ module.exports = (app, db) => {
         const note = { name: req.body.name, email:req.body.email, age: req.body.age,
 			height: req.body.height,Weight: req.body.Weight, desiredWeight : req.body.desiredWeight,
 			password:req.body.password
-		};
-		//if user name already present in mongodb alerting the user 
+        };
+        
+		//If user name already present in mongodb display the error message to the user
         db.collection('UserData').findOne({email: req.body.email}).then(function(result){
             if(!(result==null)){
                 res.status(200).json({msg:"Email Id already present"});
@@ -52,25 +53,7 @@ module.exports = (app, db) => {
         });
 	});
 
-
-	app.post("/updateWeight", (req, res) => {
-     //console.log(req.body);
-		let  myquery = { email: req.body.email };
-		console.log(myquery);
-		let newvalues = { $set: { Weight: req.body.Weight } };
-		// console.log("updated weight " + newvalues.value);
-        db.collection("UserData").updateOne(myquery,newvalues,function(err,result){
-            if(err){
-                throw err;
-            }
-            else
-            {
-				console.log("1 document updated");
-            }
-        });
-    }); 
-
-
+    //Getting the next day menu for the user
     app.post("/", (req, res) => {
         const note = { email: req.body.email, password: req.body.password };
         db.collection('UserData').findOne({email: req.body.email, password: req.body.password}).then(function(result){
@@ -84,16 +67,14 @@ module.exports = (app, db) => {
                     {
 						userData=result;
 						console.log("hello");
-						console.log("printing from userdata " + userData.Weight);
+                        console.log("printing from userdata " + userData.Weight);
+                        
 						// creating the object and with the help of object calling the class methods 
                         let userMenu = new Menu(userData, menu , menuPerDay);
                         userMenu.calculatingBmi();
 						userMenu.calculatingCaloriesPerDay(); 
 						menuPerDay = userMenu.calculateMenuPerDay();
 						eggCount = userMenu.calculatingTheRequiredCalories();
-						//console.log("printing from route");
-						//console.log(menuPerDay);
-						//console.log(eggCount);
 						res.status(200).json({msg:"User Exist", perDayMenu: menuPerDay,eggQuantity : eggCount}); 
                     } 
 				});
@@ -105,26 +86,41 @@ module.exports = (app, db) => {
         });
 	});
 
-//  if user take oneweek menu then the weight is updated in mongodb
+    // If user take oneweek menu then the weight is updated in mongodb 
     app.post("/oneweek", (req, res) => {
         console.log("message from ajax call " + req.body.message);
 		console.log(" message one week ");
 		let userUpdatedWeight = userData.Weight
         let userMenu = new Menu(userData, menu , menuPerDay);
+
+        //Calling the method to update the user weight
         updatedWeight = userMenu.ifUserTookTheMenu(numberOfDayMenuTook, userUpdatedWeight);
         userMenu.calculatingBmi();
         userMenu.calculatingCaloriesPerDay();
         menuPerDay =  userMenu.calculateMenuPerDay();
         eggCount = userMenu.calculatingTheRequiredCalories();
         console.log(" messages one week ");
-		//console.log(menuPerDay);
-		
 		res.status(200).json({msg:"one week", perDayMenu: menuPerDay, 
 		updatedWeight: updatedWeight,eggQuantity : eggCount});
-
 	});    
 
-
+    //If user took menu for 7 days then updated the user weight in mongodb
+	app.post("/updateWeight", (req, res) => {
+        let  myquery = { email: req.body.email };
+        console.log(myquery);
+        let newvalues = { $set: { Weight: req.body.Weight } };
+        // console.log("updated weight " + newvalues.value);
+        db.collection("UserData").updateOne(myquery,newvalues,function(err,result){
+            if(err){
+                throw err;
+            }
+            else
+            {
+                console.log("1 document updated");
+            }
+        });
+    }); 
+   
 	//if user take the menu then that menu is stored in mongodb and next day menu is show to user
     app.post("/tookmenu", (req, res) => {
         console.log("message from ajax call " + req.body.message);
@@ -135,12 +131,8 @@ module.exports = (app, db) => {
         menuPerDay =  userMenu.calculateMenuPerDay();
         eggCount = userMenu.calculatingTheRequiredCalories();
         console.log(" message tookmenu ");
-		//console.log(menuPerDay);
-
-		var myquery = { email: req.body.email };
-		//console.log(myquery);
-		var newvalues = { $push: { menu : menuPerDay} };
-		console.log(newvalues);
+		let myquery = { email: req.body.email };
+		let newvalues = { $push: { menu : menuPerDay} };
         db.collection("PerDayMenuData").updateOne(myquery,newvalues,function(err,result){
             if(err){
                 throw err;
@@ -153,11 +145,14 @@ module.exports = (app, db) => {
         res.status(200).json({msg:"tookmenu", perDayMenu: menuPerDay,eggQuantity : eggCount});
 	});
 
+    //If user skipped the menu then call  the class (exercise) methods
     app.post("/skipping/menu", (req, res) => {
         console.log(" message skipping the menu ");
+        //Calling the class menu methods to find the bmi
         let userMenu = new Menu(userData, menu , menuPerDay);
         let bmi =userMenu.calculatingBmi();
-        console.log(bmi);
+
+        //Calling the class exercise and passing the bmi value as the parameter to that class
         let userExercise = new Exercise(bmi);
         let running = userExercise.running()
         let cycling = userExercise.cycling()
@@ -166,5 +161,4 @@ module.exports = (app, db) => {
         console.log(running + " cyc" + cycling + " wal " + walking + " swi" + swimming +"mt");
         res.status(200).json({runningKm: running, cyclingKm: cycling, walkingKm : walking, swimmingMeter:swimming});
     });
-
 }
